3aab3b3423e754e836fb062d7394c6fcce7626ae
whitelip <2279349694@qq.com>
2024-06-12 22:52:28 +0800

    git开发测试

---
+++ src/main/java/com/pojo/User.java
@@ -0,0 +1,8 @@
+package com.pojo;
+
+public class User {
+
+    private Integer id;
+    private String uname;
+    private String pwd;
+}
---
+++ src/main/java/com/utils/Test.java
@@ -0,0 +1,8 @@
+package com.utils;
+
+public class Test {
+
+    public void test(){
+        System.out.println("test...");
+    }
+}
