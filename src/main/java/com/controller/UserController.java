5644e5aa2e63c40a5d035111dfcfc13eee5e85be
whitelip <2279349694@qq.com>
2024-06-12 10:01:24 +0800

    测试合并git

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -7,6 +7,9 @@ public class UserController {
     public void test(){
         System.out.println("1111");
         System.out.println("2222");
+
+        System.out.println("dev...");
+        System.out.println("合并测试");
     }
 
 }
