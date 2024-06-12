3c32e0748bef0244216e58bc761d9f1f4b88275c
whitelip <2279349694@qq.com>
2024-06-12 09:55:41 +0800

    测试git

---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.controller;
+
+/**
+ *
+ */
+public class UserController {
+    public void test(){
+        System.out.println("1111");
+        System.out.println("2222");
+    }
+
+}
